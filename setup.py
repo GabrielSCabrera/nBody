4469669779377d6e1c5a62609b308dd35c0a4d4e
gabrielscabrera <gabriel.sigurd.cabrera@gmail.com>
2019-11-26 03:16:53 +0100

    Update gitignore

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     name = 'nbody',
     license = 'GPLv3',
     description = 'GPU-accelerated N-Body particle simulator with visualizer',
-    version = 'v0.0.1',
+    version = 'v0.0.2',
     long_description = README,
     url = 'https://github.com/GabrielSCabrera/nBody',
     packages = packages,#setuptools.find_namespace_packages(),
